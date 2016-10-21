9a57a9a0fc4b656a1a6f4e8ed6e3196c450bad2e
yutungh <yutungh8@gmail.com>
2016-10-21 13:31:36 -0700

    Fixed Player Movement
    Camera I think should move at its own fixed speed and not lock on the
    cat?  Cat should be constrained by the camera's view so cat can't move
    off camera.

--- KTUnity/Assets/Scripts/PlayerMovement.cs
+++ KTUnity/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public int speed;
+	public float speed = 5f;
 	public int jumpHeight;
 
 	public Transform groundPoint;
@@ -20,16 +20,22 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	void Update () {
+        //Movement Speed - default speed is defined above as a float in speed.  If no keys are pressed, default speed is defined as 5.
+        //If right is pressed speed is 5 + 2 and if left is pressed it's 5 - 2
 
-		Vector2 moveDirection = new Vector2 (Input.GetAxisRaw ("Horizontal") * speed, rb2D.velocity.y);
-		rb2D.velocity = moveDirection;
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            rb2D.velocity = new Vector2(speed, rb2D.velocity.y);
+        } else if (Input.GetAxis("Horizontal") == 1)
+        {
+            rb2D.velocity = new Vector2(speed + 2, rb2D.velocity.y);
+        } else if (Input.GetAxis("Horizontal") == -1)
+        {
+            rb2D.velocity = new Vector2(speed - 2, rb2D.velocity.y);
+        }
 
+        //Jumping
 		isGrounded = Physics2D.OverlapCircle (groundPoint.position, radius, groundMask);
-
-
-		if (Input.GetAxisRaw ("Horizontal") == 1) {
-			transform.localScale = new Vector3 (1, 1, 1);
-		}
 
 		if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded) {
 			rb2D.AddForce (new Vector2 (0, jumpHeight));
