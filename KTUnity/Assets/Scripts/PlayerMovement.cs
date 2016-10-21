cce648a8251d621d1ac5065345da355e715149d2
kelseykato <kelseykato@Kelseys-MacBook-Pro-3.local>
2016-10-20 20:59:27 -1000

    running cat, playermovement script, rainbow road

---
+++ KTUnity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int speed;
+	public int jumpHeight;
+
+	Rigidbody2D rb2D;
+
+	void Start () {
+
+		rb2D = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	void Update () {
+
+		Vector2 moveDirection = new Vector2 (Input.GetAxisRaw ("Horizontal") * speed, rb2D.velocity.y);
+		rb2D.velocity = moveDirection;
+	}
+}
