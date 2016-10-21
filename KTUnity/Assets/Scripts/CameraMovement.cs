c574c0668fe9c7b12eca4370127965fe8d0039a9
kelseykato <kelseykato@Kelseys-MacBook-Pro-3.local>
2016-10-20 23:56:30 -1000

    camera movement script

---
+++ KTUnity/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform target; //what the camera is following
+	public float smoothing; //dampening effect
+
+	Vector3 offset; //difference between character and camera
+
+	float lowestY; //lowest point that camera can go
+
+	void Start () {
+	
+		offset = transform.position - target.position; //maintain diff. in offset
+
+		lowestY = transform.position.y; //y-coord of initial position of camera
+	}
+	
+	void FixedUpdate () {
+
+		Vector3 targetCamPosition = target.position + offset; //where the camera wants to be located
+
+		transform.position = Vector3.Lerp (transform.position, targetCamPosition, smoothing * Time.deltaTime);
+
+		if (transform.position.y < lowestY) {
+			transform.position = new Vector3 (transform.position.x, lowestY, transform.position.z);
+	
+		}
+	}
+}
