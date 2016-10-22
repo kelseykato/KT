6ae294bd8cca4cdd3dfd1c8efb955f94b3c19d0a
yutungh <yutungh8@gmail.com>
2016-10-22 11:09:50 -0700

    Fixed Platform Lengths, Spawn Script Added, Destroyer Script not functional
    3 spawn points for platforms.  Destroyer script is attached to camera.
    Functionality is to destroy platforms off screen to the left and to
    detect on the bottom if the player fell off.

---
+++ KTUnity/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyer : MonoBehaviour {
+
+    void onTrggerEnter2d(Collider2D other)
+    {
+        //
+        if(other.tag == "Player")
+        {
+            Debug.Break();
+            return;
+        }
+
+        if (other.gameObject.transform.parent)
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+
+}
---
+++ KTUnity/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+    //a collection of objects to spawn(Array)
+    public GameObject[] obj;
+
+    //variables to define how fast things spawn. In this case, randomly spawn between 1 and 2 seconds.
+    public float spawnMin = 1f;
+    public float spawnMax = 2f;
+	// Use this for initialization
+	void Start () {
+        Spawn();
+	}
+
+	void Spawn () {
+
+        //create random object in the obj array.  Spawns at Transform.position and Quaternion makes it so it does not rotate.
+        Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
+
+        //After a random period of time between spawnMin and spawnMax, the function calls on itself and spawns additional things in the array obj
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+	
+	}
+}
