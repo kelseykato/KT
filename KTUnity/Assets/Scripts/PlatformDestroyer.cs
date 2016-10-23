c0560ecea257cccc4cc7e1d870c2b1130a5290a4
kelseykato <kelseykato@Kelseys-MacBook-Pro-3.local>
2016-10-22 20:47:10 -1000

    different possible create/destroy method for platforms

---
+++ KTUnity/Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+	public GameObject platformDestroyPoint;
+
+	// Use this for initialization
+	void Start () {
+
+		platformDestroyPoint = GameObject.Find ("PlatformDestroyPoint");
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (transform.position.x < platformDestroyPoint.transform.position.x) {
+
+			Destroy (gameObject);
+
+		}
+	
+	}
+}
---
+++ KTUnity/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject platform;
+	public Transform genPoint;
+	public float distBetween;
+
+	private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+	
+		platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (transform.position.x < genPoint.position.x) {
+			transform.position = new Vector3 (transform.position.x + (platformWidth /2) + distBetween, transform.position.y, transform.position.z);
+
+			Instantiate (platform, transform.position, transform.rotation);
+		}
+	}
+}
+	 
