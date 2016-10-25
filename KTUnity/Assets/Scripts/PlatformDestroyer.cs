1625d52728dfc515fed4f8077199f6a7a119f500
kelseykato <kelseykato@dhcp-168-105-110-145.wireless.manoa.hawaii.edu>
2016-10-25 11:54:13 -1000

    object pooler fixed

--- KTUnity/Assets/Scripts/PlatformDestroyer.cs
+++ KTUnity/Assets/Scripts/PlatformDestroyer.cs
@@ -9,12 +9,13 @@ public class PlatformDestroyer : MonoBehaviour {
 	void Start () {
 
 		platformDestroyPoint = GameObject.Find ("PlatformDestroyPoint");
-	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
+		//if the current position of the platform is less than the position of the destroyPoint
+		//set the platform to inactive
 		if (transform.position.x < platformDestroyPoint.transform.position.x) 
 		{
 
