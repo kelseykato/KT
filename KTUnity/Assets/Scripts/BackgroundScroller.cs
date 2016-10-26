c723ef213ddf938f43b7cab9c2284467528b4a2e
yutungh <yutungh8@gmail.com>
2016-10-25 19:46:52 -0700

    Background Scroller
    I couldn't figure it out completely because the way other tutorials are
    done have the player fixated...my solution was to fixate the background
    to the camera and move the background to the left using scroll
    speed...then after a certain amount, it would tile back using the 2
    background Quads.  I feel like for an infinite runner though, it
    wouldn't get past 2 or 3 of these backgrounds at the current speed.

---
+++ KTUnity/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tileSizeX;
+    public float cameraPos;
+
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX) + Camera.main.transform.position.x;
+        float newPosition = Time.time*scrollSpeed + Camera.main.transform.position.x;
+        transform.position = startPosition + Vector3.right * newPosition;
+    }
+}
