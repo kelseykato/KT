02416b98b3a28f1a06cffe0aaf480a5246cb7098
yutungh <yutungh8@gmail.com>
2016-10-25 20:32:52 -0700

    Laser Sorta...
    Kind of a beam laser. press space to use....ALthough I used a line
    renderer, to show a beam laser, you can make this an actual projectile
    instead.

--- KTUnity/Assets/Scripts/Weapon.cs
+++ KTUnity/Assets/Scripts/Weapon.cs
@@ -6,6 +6,8 @@ public class Weapon : MonoBehaviour {
     public float fireRate = 0;
     public float Damage = 10;
     public LayerMask whatToHit;
+    private LineRenderer lineRenderer;
+
 
     float timeToFire = 0;
     Transform firePoint;
@@ -14,6 +16,9 @@ public class Weapon : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
         firePoint = transform.FindChild("FirePoint");
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.useWorldSpace = true;
 
         if (firePoint == null)
         {
@@ -24,38 +29,51 @@ public class Weapon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        Vector2 firePointPositionEnd = new Vector2(firePoint.position.x + 100, firePoint.position.y);
+
+        lineRenderer.SetPosition(0, firePointPosition);
+        lineRenderer.SetPosition(1, firePointPositionEnd);
+
         if (fireRate == 0)
         {
-            if (Input.GetButtonDown("Fire1"))
+            if (Input.GetButtonDown("Jump"))
             {
                 //if you press the fire button specified as "Fire1", it calls the shoot function defined below.
-                Shoot();
+                lineRenderer.enabled = true;
+                //Parameters for this is Origin, Direction (by subtracting the 2 variables above, you get direction), length, and what not to hit
+                RaycastHit2D hit = Physics2D.Raycast(firePointPosition, firePointPositionEnd - firePointPosition, 100, whatToHit);
+                Debug.DrawLine(firePointPosition, (firePointPositionEnd - firePointPosition) * 100, Color.cyan);
+
+
+
+                if (hit.collider != null)
+                {
+                    Debug.DrawLine(firePointPosition, hit.point, Color.red);
+                }
+            } else if (Input.GetButtonUp("Jump"))
+            {
+                lineRenderer.enabled = false;
             }
         } else {
 
             //if you hold down the fire button, it should keep shooting depending on the fire rate variable.
 
-            if (Input.GetButton("Fire1") && Time.time > timeToFire) {
+            if (Input.GetButton("Jump") && Time.time > timeToFire) {
                 timeToFire = Time.time + 1 / fireRate;
-                Shoot();
+                //Parameters for this is Origin, Direction (by subtracting the 2 variables above, you get direction), length, and what not to hit
+                RaycastHit2D hit = Physics2D.Raycast(firePointPosition, firePointPositionEnd - firePointPosition, 100, whatToHit);
+                Debug.DrawLine(firePointPosition, (firePointPositionEnd - firePointPosition) * 100, Color.cyan);
+
+
+
+                if (hit.collider != null)
+                {
+                    Debug.DrawLine(firePointPosition, hit.point, Color.red);
+                }
             }
         }
 	}
 
-    void Shoot () {
-        //Firing origin - eyes
-        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
-
-        //Firing endpoint = same y position as the origin but x value is far to the right so I added 100
-        Vector2 firePointPositionEnd = new Vector2(firePoint.position.x + 100, firePoint.position.y);
-
-        //Parameters for this is Origin, Direction (by subtracting the 2 variables above, you get direction), length, and what not to hit
-        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, firePointPositionEnd - firePointPosition, 100, whatToHit);
-        Debug.DrawLine(firePointPosition, (firePointPositionEnd - firePointPosition) * 100, Color.cyan);
-        if (hit.collider !=null)
-        {
-            Debug.DrawLine(firePointPosition, hit.point, Color.red);
-        }
-    }
 
 }
