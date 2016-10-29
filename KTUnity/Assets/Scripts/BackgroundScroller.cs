89696d735ab5c9a63fea0784e7e82969d0bb61a6
yutungh <yutungh8@gmail.com>
2016-10-29 10:22:57 -0700

    Added Circle Collider to Asteroid...BG scroller not functional. need to fix that...

--- KTUnity/Assets/Scripts/BackgroundScroller.cs
+++ KTUnity/Assets/Scripts/BackgroundScroller.cs
@@ -16,18 +16,23 @@ public class BackgroundScroller : MonoBehaviour {
         startPosition = transform.position;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         //float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX) + Camera.main.transform.position.x;
-        float newPosition = Time.time*scrollSpeed + Camera.main.transform.position.x;
+        float newPosition = Time.time * scrollSpeed + Camera.main.transform.position.x;
         transform.position = startPosition + Vector3.right * newPosition;
-
-        if (Camera.main.transform.position.x - transform.position.x > 300)
+        if (Camera.main.transform.position.x - transform.position.x > 600)
         {
-            transform.position = Vector3.right * 1204;
+            bgOffset();
         }
-
     }
+
+    void bgOffset (){
+        transform.position = Vector3.right * 1400;
+        startPosition = transform.position;
+    }
+
 
 }
