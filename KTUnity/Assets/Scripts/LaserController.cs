f923409ed89a79e52d07ae63aac7c94bdb05ff95
yutungh <yutungh8@gmail.com>
2016-11-15 17:46:01 -0800

    Possible Laser Fix
    a positional check added in to the laser so if it hits anything and
    after a certain amount of time the position is still the same, it sets
    the laser projectile to inactive.

--- KTUnity/Assets/Scripts/LaserController.cs
+++ KTUnity/Assets/Scripts/LaserController.cs
@@ -7,16 +7,42 @@ public class LaserController : MonoBehaviour {
     public float speed;
     Rigidbody2D rigid;
 
+    private float positionA;
+    private float positionB;
+    private bool positionState;
+    public float positionCheckDelay;
+    private float startTime;
+
 
     // Use this for initialization
     void Start () {
         rigid = GetComponent<Rigidbody2D>();
         laserDestroyPoint = GameObject.Find("PlatformGenPoint");
+        positionState = false;
+        positionA = transform.position.x;
+        startTime = Time.time;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
         rigid.velocity = new Vector2(speed, rigid.velocity.y);
+
+        if (Time.time - startTime > positionCheckDelay)
+        {
+            positionB = transform.position.x;
+            if (positionB == positionA)
+            {
+                gameObject.SetActive(false);
+            } else
+            {
+                startTime = Time.time;
+                positionA = transform.position.x;
+            }
+        }
+
+
+
 	
 	}
 
